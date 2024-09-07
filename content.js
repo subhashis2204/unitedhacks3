b5503b0856f25b3d13f88289fb3eb376c45f48b4
subhashis2204 <subhashispaul2204@gmail.com>
2024-09-07 23:47:51 +0530

    fixed time related bugs

--- content.js
+++ content.js
@@ -1,21 +1,25 @@
 chrome.runtime.sendMessage({ todo: "showPageAction" });
 
 window.addEventListener('load', (event) => { // run once window has finished loading
+    if (chrome.runtime.id == undefined) return; // to prevent Uncaught Error: Extension context invalidated
     renderTab();
     renderWindow();
     renderDim();
     loadTimer();
 });
 
-var currentTime = -2; // in seconds; -1 means new timer, -2 means new page load
+const defaultBreakInterval = 25 * 60;
+const defaultBreakDuration = 5 * 60;
+
+var currentState; // 0: work, 1: break
+var currentTime = -1; // in seconds
 var intervalTimer;
 
-var savedState = -1; // 0: work, 1: break
-var cachedTime = -1;
-
-function renderTab() { // render tab for timer display
-    var pixelFont = new FontFace('Pixeloid Sans', 'url("../assets/fonts/PixeloidSans.woff")', { style: 'normal', weight: 'normal' });
-    document.fonts.add(pixelFont);
+async function renderTab() { // render tab for timer display
+    var pixelFont = document.createElement('style');
+    pixelFont.textContent = '@font-face { font-family: Pixeloid Sans;'
+                            + 'src: url("' + chrome.runtime.getURL('assets/fonts/PixeloidSans.woff') + '"); }';
+    document.head.appendChild(pixelFont);
 
     let tabDiv = document.createElement("div");
     tabDiv.setAttribute("id", "crittersbreak-tab");
@@ -24,10 +28,10 @@ function renderTab() { // render tab for timer display
     tabDiv.innerHTML = '<p id="crittersBreak-timeDisplay"></p>'
     // style settings
     // body
-    tabDiv.style.backgroundColor = "#ffffff";
-    tabDiv.style.color = "#303030";
+    tabDiv.style.backgroundColor = "#7d4d83";
+    tabDiv.style.color = "#fff";
     tabDiv.style.fontFamily = "Pixeloid Sans";
-    tabDiv.style.fontSize = "36px";
+    tabDiv.style.fontSize = "20px";
     tabDiv.style.position = "fixed";
     tabDiv.style.right = "0px";
     tabDiv.style.top = "200px";
@@ -35,11 +39,34 @@ function renderTab() { // render tab for timer display
     tabDiv.style.height = "60px";
     tabDiv.style.borderRadius = "5px 0 0 5px";
     tabDiv.style.display = "flex";
+    tabDiv.style.flexDirection = "column";
     tabDiv.style.justifyContent = "center";
     tabDiv.style.alignItems = "center";
     tabDiv.style.userSelect = "none";
     tabDiv.style.zIndex = 2147483646;
     tabDiv.style.cursor = "default";
+
+    let tabText = document.createElement("p");
+    tabText.setAttribute("id", "crittersbreak-tabText");
+    tabDiv.appendChild(tabText);
+    tabText.innerHTML = "Working...";
+    tabText.style.margin = "0";
+    tabText.style.fontSize = "14px";
+
+    let tabIcon = document.createElement("div");
+    tabIcon.setAttribute("id", "crittersbreak-tabIcon");
+    tabDiv.appendChild(tabIcon);
+    tabIcon.innerHTML = "<img src=\""+ chrome.runtime.getURL('assets/critters/sir-teddy/idle/idle-1.png') + "\">";
+    tabIcon.getElementsByTagName("img")[0].style.height = "30px";
+    tabIcon.style.backgroundColor = "#3B1F3F"
+    tabIcon.style.width = "32px";
+    tabIcon.style.height = "32px";
+    tabIcon.style.borderRadius = "100%";
+    tabIcon.style.border = "solid 2px #fff"
+    tabIcon.style.position = "absolute";
+    tabIcon.style.top = "-12px";
+    tabIcon.style.left = "-12px";
+    tabIcon.style.display = "none";
 
     document.getElementById("crittersBreak-timeDisplay").style.margin = "0";
 }
@@ -92,7 +119,7 @@ function renderDim() { // div for dimming screen
 async function dimScreen() {
     clearInterval(intervalTimer);
     const result = await getChromeSettings();
-    if (result?.settings?.dimming == true) {
+    if (result.settings == undefined || result?.settings?.dimming == true) {
         var elDim = document.getElementById("crittersbreak-dim");
         elDim.style.display = 'block';
         var op = parseFloat(elDim.style.opacity);  // initial opacity
@@ -118,8 +145,14 @@ function brightenScreen() {
         }
         elDim.style.opacity = op;
         elDim.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op -= 0.05;
+        op -= 0.1;
     }, 50);
+}
+
+function toggleWindow() {
+    var elWindow = document.getElementById("crittersbreak-window");
+    if (elWindow.style.display == 'none') revealWindow();
+    else hideWindow();
 }
 
 function revealWindow() {
@@ -155,87 +188,88 @@ function hideWindow() {
 }
 
 async function loadTimer() {
-    const result = await getChromeCurrData();
-    if (result?.currentData?.timeLeft != null && result?.currentData?.timeLeft != undefined) {
-        savedState = result.currentData.state;
-        cachedTime = result.currentData.timeLeft;
-    } else {
-        savedState = 0;
-        cachedTime = await stateToTime(savedState);
+    var result = await getChromeCurrData();
+    if (result?.currentData?.timeLeft != null && result?.currentData?.timeLeft != undefined && result?.currentData?.timeLeft != NaN) { // if saved data from another page exists
+        currentState = result.currentData.state;
+        currentTime = result.currentData.timeLeft;
+    } else { // use defaults
+        currentState = 0;
+        currentTime = await stateToTime(currentState); // i.e. default break interval
     }
-    startTimer();
+    startTimer(currentState, currentTime);
 }
 
-async function startTimer() {
-    var initialTime = (currentTime == -2) ? cachedTime : await stateToTime(savedState);
+async function startTimer(state, time) {
     const currentData = {};
-    currentData.state = savedState;
-    let timer = setInterval(function () {
-        if (currentTime <= -1) currentTime = initialTime;
-
-        currentData.timeLeft = currentTime;
+    currentData.state = state;
+    let timer = setInterval(async function () {
+        if (chrome.runtime.id == undefined) return; // to prevent Uncaught Error: Extension context invalidated
+        currentData.timeLeft = time;
         chrome.storage.session.set({currentData});
 
-        let minutesNum = (currentTime - (currentTime % 60)) / 60;
+        // Calculate minutes
+        let minutesNum = (time - (time % 60)) / 60;
         var minutesStr = "" + minutesNum;
         if (minutesNum == 0) minutesStr = "00"
         else if (minutesNum < 10) minutesStr = "0" + minutesStr;
     
-        let secondsNum = currentTime - (minutesNum * 60);
+        // Calculate seconds
+        let secondsNum = time - (minutesNum * 60);
         var secondsStr = "" + secondsNum;
         if (secondsNum == 0) secondsStr = "00"
         else if (secondsNum < 10) secondsStr = "0" + secondsStr;
     
+        // Update display with time
         if (document.getElementById("crittersBreak-timeDisplay") != null) document.getElementById("crittersBreak-timeDisplay").innerHTML = minutesStr + ":" + secondsStr;
     
-        if (currentTime > 0) currentTime--;
+        if (time > 0) time--;
         else { // timer goes to zero
-            if (savedState == 0) { // transition to break
-                savedState = 1;
+            if (state == 0) { // transition to break
+                state = 1;
                 document.getElementById("crittersbreak-tab").style.cursor = "pointer";
-                document.getElementById("crittersBreak-timeDisplay").innerHTML = "Break";
-                document.getElementById("crittersbreak-tab").addEventListener("click", revealWindow);
+                document.getElementById("crittersbreak-tabText").innerHTML = "Take a break!";
+                document.getElementById("crittersbreak-tabIcon").style.display = "block";
+                document.getElementById("crittersbreak-tab").addEventListener("click", toggleWindow);
 
                 document.getElementById("crittersbreak-window").addEventListener("mouseover", dimScreen);
                 document.getElementById("crittersbreak-window").addEventListener("mouseout", brightenScreen);
-                startTimer();
             } else { // transition to work
-                savedState = 0;
+                state = 0;
                 document.getElementById("crittersbreak-tab").style.cursor = "default";
-                document.getElementById("crittersBreak-timeDisplay").innerHTML = "Work";
-                document.getElementById("crittersbreak-tab").removeEventListener("click", revealWindow);
+                document.getElementById("crittersbreak-tabText").innerHTML = "Working...";
+                document.getElementById("crittersbreak-tabIcon").style.display = "none";
+                document.getElementById("crittersbreak-tab").removeEventListener("click", toggleWindow);
 
                 document.getElementById("crittersbreak-window").removeEventListener("mouseover", dimScreen);
                 document.getElementById("crittersbreak-window").removeEventListener("mouseout", brightenScreen);
 
                 brightenScreen();
                 hideWindow();
-                startTimer();
             }
-            
-            currentTime = -1;
+
             clearInterval(timer);
+            startTimer(state, await stateToTime(state));
         }
     }, 1000);
 }
 
-async function stateToTime(state) {
+async function stateToTime(state) { // retrieves the duration for the given state, based on user settings
     var result = await getChromeSettings();
-    if (state == 0) { // break interval
-        return parseInt(result?.settings?.breakInterval * 60) ?? (5);
-    } else { // break duration
-        return parseInt(result?.settings?.breakDuration * 60) ?? (3);
+    if (state == 0) { // work: get break interval from user settings or use default
+        return (result.settings != undefined) ? parseInt(result.settings.breakInterval * 60) : defaultBreakInterval;
+    } else { // break: get break duration from user settings or get default
+        return (result.settings != undefined) ? parseInt(result.settings.breakDuration * 60) : defaultBreakDuration;
     }
 }
 
-const getChromeCurrData = () =>
+const getChromeCurrData = () => // Get the saved state and timing (from other pages) from Chrome storage
     new Promise(function (resolve) {
         chrome.storage.session.get("currentData", function (result) {
             resolve(result);
         });
     });
 
-const getChromeSettings = () =>
+const getChromeSettings = () => // Get user setings from Chrome storage
     new Promise(function (resolve) {
         chrome.storage.sync.get("settings", function (result) {
             resolve(result);
